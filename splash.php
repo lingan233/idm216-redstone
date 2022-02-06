3e8b9971de0b596dc001ce138304478a75c0092b
rogerkorp <71797131+rogerkorp@users.noreply.github.com>
2022-02-06 11:23:21 -0500

    :ocean: Added the Splash Page!
    Introduced a simple splash page into the project. Added in some additional CSS rules to accompany it.

---
+++ splash.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REDSTONE PIZZA</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body id="splash-page">
+    <img src="https://via.placeholder.com/200">
+</body>
+</html>
