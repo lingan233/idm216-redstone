33bb8d9bfc3101e55e2595e5b1213ba265f083a1
rogerkorp <71797131+rogerkorp@users.noreply.github.com>
2022-02-06 14:43:35 -0500

    :sparkles: Pick-up Details built!
    Added in a few CSS rules for the pick-up details page!

--- ready-pickup.php
+++ ready-pickup.php
@@ -1,4 +1,35 @@
-<h2>Ready for Pickup!</h2>
-<h3>Order #: </h3>
-<h3>Pick-up Time: ASAP</h3>
-<h3>Pick-up Location: 3315 Market St, Philadelphia, PA 19104</h3>
+
+
+
+<?php
+include_once __DIR__ . '/header.php';
+?>
+<main class="wrapper">
+    <img id="pizza-pickup" src="https://via.placeholder.com/300" alt="map">
+    <section id="pickup-details">
+
+        <h1>Ready for Pickup!</h1>
+
+        <div id="pickup-number">
+            <h2>Order #: </h2>
+            <p id="order-number">A7F32</p>
+        </div>
+
+        <div id="pickup-time">
+            <h2>Pick-up Time:</h2>
+            <p id="order-time">ASAP</p>
+        </div>
+
+        <div id="pickup-locale">
+            <h2>Pick-up Location:</h2>
+            <br>
+            <p id="order-locale">3315 Market St, Philadelphia, PA 19104</p>
+        </div>
+
+    </section>  
+    <button>Done</button>
+</main>
+
+<?php
+include_once __DIR__ . '/footer.php';
+?>
