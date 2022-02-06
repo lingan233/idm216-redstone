56752bc036696cdff1e5917c129b4fec8ba88717
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-02-06 01:20:43 -0500

    added footer.php and updated select-base.php

--- front.php
+++ front.php
@@ -1,13 +1,14 @@
 <?php
 include_once __DIR__ . '/header.php';
 ?>
-    <main class="wrapper">
-        <h1>Good Afternoon!</h1>
-        <p class="large-text">Today we're at Drexel University from 11AM to 5PM</p>
-        <img class="map" src="imgs/map-placeholder.png" alt="map">
-        <button>Start a New Order</button>
-        <button>Review Order History</button>
-    </main>
-</body>
+<main class="wrapper">
+    <h1>Good Afternoon!</h1>
+    <p class="large-text">Today we're at Drexel University from 11AM to 5PM</p>
+    <img class="map" src="imgs/map-placeholder.png" alt="map">
+    <button>Start a New Order</button>
+    <button>Review Order History</button>
+</main>
 
-</html>
+<?php
+include_once __DIR__ . '/footer.php';
+?>
--- select-base.php
+++ select-base.php
@@ -1,4 +1,16 @@
-<h2>Select a Base</h2>
-<button>Classic Red</button>
-<button>White</button>
-<button>Stuffed Crust</button>
+<?php
+include_once __DIR__ . '/header.php';
+?>
+<main class="wrapper select-base">
+    <h1>Select a Base</h1>
+    <img src="https://via.placeholder.com/300" alt="">
+    <button>Classic Red</button>
+    <img src="https://via.placeholder.com/300" alt="">
+    <button>White</button>
+    <img src="https://via.placeholder.com/300" alt="">
+    <button>Stuffed Crust</button>
+</main>
+
+<?php
+include_once __DIR__ . '/footer.php';
+?>
