469bde717cf276b153b43d5eb3b07c8ea6780325
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-02-07 15:05:27 -0500

    Update payment.php

--- payment.php
+++ payment.php
@@ -6,7 +6,7 @@ include_once __DIR__ . '/header.php';
     <h2>Review Order Details</h2>
 
     <?php
-            include __DIR__ . '/order-item.php';
+    include __DIR__ . '/order-item.php';
     ?>
     <section id="debit-card">
         <div id="card-thumbnail">
@@ -18,55 +18,55 @@ include_once __DIR__ . '/header.php';
             <p id="card-number"> 1234 **** **** ****</p>
         </div>
     </section>
-<br>
+    <br>
     <a href="">Use a different payment method?</a>
     <div id="order-details">
-        
+
         <div id="subtotal-details">
             <h4>Subtotal</h4>
             <p>$12.00</p>
         </div>
-        
-        <div id="tax-details"> 
+
+        <div id="tax-details">
             <h4>Tax & Fees</h4>
             <p>$2.75</p>
         </div>
-        
+
         <div id="tip-details">
             <h4>Tips</h4>
-                <div id="tip-choose">
-                    <label class="tip-label"  for="0-percent-tip">
-                        <input type="radio" id="0-percent-tip" name="tip-amount" value="$0.00">
-                        <div class="tip-input" id="first-box">
-                            <p>0%</p>
-                            <p>$0.00</p>
-                        </div>
-                    </label>
+            <div id="tip-choose">
+                <label class="tip-label" for="0-percent-tip">
+                    <input type="radio" id="0-percent-tip" name="tip-amount" value="$0.00">
+                    <div class="tip-input" id="first-box">
+                        <p>0%</p>
+                        <p>$0.00</p>
+                    </div>
+                </label>
 
-                    <label class="tip-label" for="10-percent-tip">
-                        <input type="radio" id="10-percent-tip" name="tip-amount" value="$1.00">
-                        <div class="tip-input"> 
-                            <p>10%</p>
-                            <p>$1.00</p>
-                        </div> 
-                    </label>
+                <label class="tip-label" for="10-percent-tip">
+                    <input type="radio" id="10-percent-tip" name="tip-amount" value="$1.00">
+                    <div class="tip-input">
+                        <p>10%</p>
+                        <p>$1.00</p>
+                    </div>
+                </label>
 
-                    <label class="tip-label" for="15-percent-tip">
-                        <input type="radio" id="15-percent-tip" name="tip-amount" value="$1.50">
-                        <div class="tip-input">
-                            <p>15%</p>
-                            <p>$1.50</p>
-                        </div>
-                    </label>
+                <label class="tip-label" for="15-percent-tip">
+                    <input type="radio" id="15-percent-tip" name="tip-amount" value="$1.50">
+                    <div class="tip-input">
+                        <p>15%</p>
+                        <p>$1.50</p>
+                    </div>
+                </label>
 
-                    <label class="tip-label" for="20-percent-tip">
-                        <input type="radio" id="20-percent-tip" name="tip-amount" value="$2.00">
-                        <div class="tip-input" id="last-box"> 
-                            <p>20%</p>
-                            <p>$2.00</p>
-                        </div> 
-                    </label>
-                </div>
+                <label class="tip-label" for="20-percent-tip">
+                    <input type="radio" id="20-percent-tip" name="tip-amount" value="$2.00">
+                    <div class="tip-input" id="last-box">
+                        <p>20%</p>
+                        <p>$2.00</p>
+                    </div>
+                </label>
+            </div>
         </div>
         <a id="custom-tip" href="">Enter custom amount</a>
 
@@ -74,8 +74,12 @@ include_once __DIR__ . '/header.php';
             <h3>Total</h3>
             <p>$2.75</p>
         </div>
-</div>
-    <button>Confirm Order</button>
+    </div>
+
+    <a href="ready-pickup.php">
+        <button>Confirm Order</button>
+    </a>
+
 </main>
 <?php
 include_once __DIR__ . '/footer.php';
