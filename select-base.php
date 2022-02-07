7146d73a05e578853f753e998683cfc1fb629723
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-02-07 14:38:23 -0500

    Update select-base.php

--- select-base.php
+++ select-base.php
@@ -4,7 +4,10 @@ include_once __DIR__ . '/header.php';
 <main class="wrapper select-base">
     <h1>Select a Base</h1>
     <img src="https://via.placeholder.com/300" alt="">
-    <button>Classic Red</button>
+    <a href="customize-pizza.php">
+        <button>Classic Red</button>
+    </a>
+
     <img src="https://via.placeholder.com/300" alt="">
     <button>White</button>
     <img src="https://via.placeholder.com/300" alt="">
