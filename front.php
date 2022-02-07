e2df77016bfb96fdcfd7da7cda2541af3c4aeb87
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-02-07 14:23:24 -0500

    Update front.php

--- front.php
+++ front.php
@@ -5,8 +5,13 @@ include_once __DIR__ . '/header.php';
     <h1>Good Afternoon!</h1>
     <p class="large-text">Today we're at Drexel University from 11AM to 5PM</p>
     <img class="map" src="imgs/map-placeholder.png" alt="map">
-    <button>Start a New Order</button>
-    <button>Review Order History</button>
+    <a href="front-pick-up.php">
+        <button>Start a New Order</button>
+    </a>
+    <a href="">
+        <button>Review Order History</button>
+    </a>
+
 </main>
 
 <?php
