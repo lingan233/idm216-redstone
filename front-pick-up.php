6b7883a147302ad5d29a8230520843a861b3796c
rogerkorp <71797131+rogerkorp@users.noreply.github.com>
2022-02-07 20:56:44 -0500

    :construction: Fixed formatting on front page
    Fixed up the PHP so that the front page will display correctly. Fixed up some minor text issues.

--- front-pick-up.php
+++ front-pick-up.php
@@ -1,12 +1,17 @@
 <?php
 include_once __DIR__ . '/header.php';
 ?>
+
 <main class="wrapper">
-
-
-<?php
-include_once __DIR__ . '/front.php';
-?>
+    <h1>Good Afternoon!</h1>
+    <p class="large-text">Today we're at Drexel University from 11AM to 5PM</p>
+    <img class="map" src="imgs/map-placeholder.png" alt="map">
+    <a href="front-pick-up.php">
+        <button>Start a New Order</button>
+    </a>
+    <a href="">
+        <button>Review Order History</button>
+    </a>
             <div class="pop-up white-background">
                 <a href="front.php" class="close"><img src="https://via.placeholder.com/32"></a>
                 <h2>Pick Up Times</h2>
@@ -17,7 +22,6 @@ include_once __DIR__ . '/front.php';
                 <a class="false-pick-up-time">11:30 AM</a>
                 <a href="menu.php" id="confirm-pick-up-time">Confirm</a>
             <div>
-</main>
 
 <?php
 include_once __DIR__ . '/footer.php';
