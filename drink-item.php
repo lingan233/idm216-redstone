b9e8ab230c91716e0b9eb83cf7fcc73368af5993
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-02-08 17:00:56 -0500

    Made pizza-item clickable

--- drink-item.php
+++ drink-item.php
@@ -1,10 +1,11 @@
-<section class="drink-container">
-    <div class="drink-img">
-        <img src="imgs/menu/<?php echo $itemImg; ?>" alt=""></img>
-    </div>
-    <div class="menu-item-text">
-        <h3><?php echo $itemName; ?></h3>
-        <p class="price orange-text"><?php echo $itemPrice; ?></p>
-    </div>
-
-</section>
+<!-- <a href="" class="no-underline"> -->
+    <section class="drink-container">
+        <div class="drink-img">
+            <img src="imgs/menu/<?php echo $itemImg; ?>" alt=""></img>
+        </div>
+        <div class="menu-item-text">
+            <h3><?php echo $itemName; ?></h3>
+            <p class="price orange-text"><?php echo $itemPrice; ?></p>
+        </div>
+    </section>
+<!-- </a> -->
--- pizza-item.php
+++ pizza-item.php
@@ -1,12 +1,12 @@
-
-<section>
-    <div>
-        <img src="imgs/menu/<?php echo $itemImg; ?>" alt=""></img>
-    </div>
-    <div class="menu-item-text">
-        <h3><?php echo $itemName; ?></h3>
-        <p><?php echo $itemDescription; ?></p>
-        <p class="price orange-text"><?php echo $itemPrice; ?></p>
-    </div>
-
-</section>
+    <a href="select-base.php" class="no-underline">
+        <section>
+            <div>
+                <img src="imgs/menu/<?php echo $itemImg; ?>" alt=""></img>
+            </div>
+            <div class="menu-item-text">
+                <h3><?php echo $itemName; ?></h3>
+                <p><?php echo $itemDescription; ?></p>
+                <p class="price orange-text"><?php echo $itemPrice; ?></p>
+            </div>
+        </section>
+    </a>
