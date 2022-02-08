67406bd6748795b91d41fa7c18f346276f690ca8
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-02-08 17:07:04 -0500

    menu-with-cart.php added

---
+++ menu-with-cart.php
@@ -0,0 +1,9 @@
+<?php
+include_once __DIR__ . '/menu.php';
+?>
+
+<a href="select-base.php">
+    <button class="cart-button">Cart
+        <div>1</div>
+    </button>
+</a>
--- menu.php
+++ menu.php
@@ -121,11 +121,11 @@ include_once __DIR__ . '/header.php';
         ?>
 
 
-        <a href="select-base.php">
+        <!-- <a href="select-base.php">
             <button class="cart-button">Cart
                 <div>1</div>
             </button>
-        </a>
+        </a> -->
 
     </div>
 </main>
