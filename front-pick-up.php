5afdd3027ef411c88cf9a922142c572ea16b640e
rogerkorp <71797131+rogerkorp@users.noreply.github.com>
2022-02-08 16:57:26 -0500

    :art: Added SVG cross

--- front-pick-up.php
+++ front-pick-up.php
@@ -6,14 +6,10 @@ include_once __DIR__ . '/header.php';
     <h1>Good Afternoon!</h1>
     <p class="large-text">Today we're at Drexel University from 11AM to 5PM</p>
     <img class="map" src="imgs/map-placeholder.png" alt="map">
-    <a href="front-pick-up.php">
-        <button>Start a New Order</button>
-    </a>
-    <a href="">
-        <button>Review Order History</button>
-    </a>
+    <a href="front-pick-up.php" class="front-link">Start a New Order</a>
+    <a href="" class="front-link">Review Order History</a>
             <div class="pop-up white-background">
-                <a href="front.php" class="close"><img src="https://via.placeholder.com/32"></a>
+                <a href="front.php" class="close"><img src="svg/ui-cross.svg" height="20" width="20"></a>
                 <h2>Pick Up Times</h2>
                 <a class="true-pick-up-time">ASAP</a>
                 <a class="false-pick-up-time">10:45 AM</a>
