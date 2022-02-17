863162a4e77ebc2455fa6f79b36601ea66b11782
rogerkorp <71797131+rogerkorp@users.noreply.github.com>
2022-02-17 17:16:18 -0500

    :sparkles: Account Creation Template
    - Made a simple account creation screen for the Beta.

---
+++ create-account.php
@@ -0,0 +1,22 @@
+<?php
+include_once __DIR__ . '/header.php';
+?>
+
+<div class="welcome-text-on-front-page">
+    <h1>Create your Account!</h1>
+    <p class="large-text"> </p>
+</div>
+<main class="wrapper">
+    <div class="sign-up-box">
+        <form>
+            <input type="text" placeholder="Username"><br>
+            <input type="password" placeholder="Password"><br>
+            <input type="password" placeholder="Confirm Password"><br>
+            <input type="text" placeholder="Email"><br>
+            <button>Create</button>
+        </form>
+    </div class="login">
+
+<?php
+include_once __DIR__ . '/footer.php';
+?>
--- micro-cart-number-update.php
+++ micro-cart-number-update.php
@@ -1,17 +1,17 @@
 <?php
 include_once __DIR__ . '/header.php';
 ?>
+<main class="wrapper">
+    <div id="order-shopping-cart">
+        <div id="order-shopping-cart-number"></div>
+    </div>
 
-<div id="order-shopping-cart">
-    <div id="order-shopping-cart-number"></div>
-</div>
-
-<button id="add-to-cart-button">Add Item</button>
-<button id="remove-from-cart-button">Remove Item</button>
-<button id="clear-storage">Clear Session Storage</button>
+    <button id="add-to-cart-button">Add Item</button>
+    <button id="remove-from-cart-button">Remove Item</button>
+    <button id="clear-storage">Clear Session Storage</button>
 
 
-<a href="micro-cart-number-update-storage-test.php">Please work!</a>
+    <a href="micro-cart-number-update-storage-test.php">Please work!</a>
 
 <script type="text/javascript" src="cart-number-script.js"></script>
 
