23747c93037a653ce7818ca9a34cf79926a58a74
rogerkorp <71797131+rogerkorp@users.noreply.github.com>
2022-02-17 13:09:49 -0500

    :sparkles: Template for Shopping Cart Micro
    - Added a simple PHP file to work on the shopping cart micro-interaction.

---
+++ micro-cart-number-update.php
@@ -0,0 +1,11 @@
+<?php
+include_once __DIR__ . '/header.php';
+?>
+
+<div class="order-shopping-cart">
+    <div class="order-shopping-cart-number">X</div>
+</div>
+
+<?php
+include_once __DIR__ . '/footer.php';
+?>
