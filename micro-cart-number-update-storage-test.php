ff90e61a845dfdba1f0aec73ba916b02a93f6551
rogerkorp <71797131+rogerkorp@users.noreply.github.com>
2022-02-17 15:51:26 -0500

    :construction: Working on Session Data Perseverance
    - Data currently refreshes in between pages.
    
    - Need to look into how to ensure that the data can update across the entire session.

---
+++ micro-cart-number-update-storage-test.php
@@ -0,0 +1,14 @@
+<?php
+include_once __DIR__ . '/header.php';
+?>
+
+    <div id="order-shopping-cart-number"></div>
+
+
+
+
+<script type="text/javascript" src="cart-number-script.js"></script>
+
+<?php
+include_once __DIR__ . '/footer.php';
+?>
--- micro-cart-number-update.php
+++ micro-cart-number-update.php
@@ -2,9 +2,18 @@
 include_once __DIR__ . '/header.php';
 ?>
 
-<div class="order-shopping-cart">
-    <div class="order-shopping-cart-number">X</div>
+<div id="order-shopping-cart">
+    <div id="order-shopping-cart-number"></div>
 </div>
+
+<button id="add-to-cart-button">Add Item</button>
+<button id="remove-from-cart-button">Remove Item</button>
+<button id="clear-storage">Clear Session Storage</button>
+
+
+<a href="micro-cart-number-update-storage-test.php">Please work!</a>
+
+<script type="text/javascript" src="cart-number-script.js"></script>
 
 <?php
 include_once __DIR__ . '/footer.php';
