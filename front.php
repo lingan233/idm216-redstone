844dcf9db5c661935fa395a2e9beaa6959be237e
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-02-21 16:44:28 -0500

    order-history added

--- front.php
+++ front.php
@@ -9,7 +9,7 @@ include_once __DIR__ . '/header.php';
 <main class="wrapper">
     <img class="map" src="imgs/map-placeholder.png" alt="map">
     <a href="front-pick-up.php" class="button">Start a New Order</a>
-    <a href="" class="button less-top-margin">Review Order History</a>
+    <a href="order-history.php" class="button less-top-margin">Review Order History</a>
 
 <?php
 include_once __DIR__ . '/footer.php';
