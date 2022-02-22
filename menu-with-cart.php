bbc9d2c2d171a6de35383a15f97c44dce1206fe5
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-02-22 17:08:00 -0500

    cart button update

--- menu-with-cart.php
+++ menu-with-cart.php
@@ -2,6 +2,7 @@
 include_once __DIR__ . '/menu.php';
 ?>
 
-<a href="cart.php" class="button cart-button no-underline">Cart
+<a href="cart.php" class="button cart-button no-underline">
+        Cart
         <div>1</div>
 </a>
