3cfd40c6b5ad190b9fe363683d1cc07654409769
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-02-23 00:02:54 -0500

    Update customize-tip.php

--- customize-tip.php
+++ customize-tip.php
@@ -4,7 +4,7 @@ include_once __DIR__ . '/header.php';
 
 <div class="wrapper">
     <div class="flex-container">
-        <form action="payment.php" class="customize-tip">
+        <form action="payment.php" class="customize-tip" method="post">
             <h1>Customize Your Tip</h1>
             <input type="number" id="quantity" name="quantity" min="0" max="100">
             <label for="percent">%</label>
