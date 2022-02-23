dbc17c05063e6f6194b0b5b9debe1bc998a90316
rogerkorp <71797131+rogerkorp@users.noreply.github.com>
2022-02-22 22:37:31 -0500

    :sparkles: Fixed the Pick-Up Time
    The pick-up time box now uses the proper microinteractions that denote when a time is selected or not.
    
    Like the tip selectors, it's essentially a glorified radio button selector, so we would have to have some values pre-written into the options for JS or PHP to actually use them
    
    For the purposes of a demo, however, they should do fine.

--- pick-up-time.php
+++ pick-up-time.php
@@ -4,16 +4,43 @@ include_once __DIR__ . '/payment.php';
 ?>
 
 <div class="wrapper">
-    <div class="pop-up white-background">
-        <a href="front.php" class="close"><img src="svg/ui-cross.svg" height="20" width="20"></a>
-        <h2>Pick Up Times</h2>
-        <a class="true-pick-up-time">ASAP</a>
-        <a class="false-pick-up-time">10:45 AM</a>
-        <a class="false-pick-up-time">11:00 AM</a>
-        <a class="false-pick-up-time">11:15 AM</a>
-        <a class="false-pick-up-time">11:30 AM</a>
-        <a href="ready-pickup.php" class="small-button" id="confirm-pick-up-time">Confirm</a>
-    </div>
+<div class="pop-up white-background">
+                <a href="front.php" class="close"><img src="svg/ui-cross.svg" height="20" width="20"></a>
+                <h2>Pick Up Times</h2>
+                <form id="pick-up-time">
+                    <label class="pick-up-time" for="asap">
+                        <input type="radio" id="asap" name="time-choices">
+                        <div class="pick-up-input">
+                            <p>ASAP</p>
+                        </div>
+                    </label>
+                    <label class="pick-up-time" for="time-one">
+                        <input type="radio" id="time-one" name="time-choices">
+                        <div class="pick-up-input">
+                            <p>10:45 AM</p>
+                        </div>
+                    </label>
+                    <label class="pick-up-time" for="time-two">
+                        <input type="radio" id="time-two" name="time-choices">
+                        <div class="pick-up-input">
+                            <p>11:00 AM</p>
+                        </div>
+                    </label>
+                    <label class="pick-up-time" for="time-three">
+                        <input type="radio" id="time-three" name="time-choices">
+                        <div class="pick-up-input">
+                            <p>11:15 AM</p>
+                        </div>
+                    </label>
+                    <label class="pick-up-time" for="time-four">
+                        <input type="radio" id="time-four" name="time-choices">
+                        <div class="pick-up-input">
+                            <p>11:30 AM</p>
+                        </div>
+                    </label>
+                    <a href="ready-pickup.php" class="big-red-button" id="confirm-pick-up-time">Confirm</a>
+                </form>
+            </div>
 </div>
 
 <!-- <div class="wrapper">
