1b792f249aeeefc4f0fb0d1f6127aed78a792792
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-03-14 19:55:50 -0400

    menu page responsive

--- menu.php
+++ menu.php
@@ -14,8 +14,8 @@ include_once __DIR__ . '/header.php';
             </a>
         </div>
 
+        <h2 class="menu-subhead">Pizza (10-Inch) </h2>
         <div class="menu-container">
-            <h2 class="menu-subhead">Pizza (10-Inch) </h2>
             <?php
             $itemLink = 'select-base.php';
             $itemImg = 'plain-pizza.png';
@@ -28,16 +28,17 @@ include_once __DIR__ . '/header.php';
             ?>
         </div>
 
+        <h2 class="menu-subhead">Salads</h2>
         <div class="menu-container">
-            <h2 class="menu-subhead">Salads</h2>
             <?php
             include 'other-item.php';
             mysqli_free_result($other_result);
             ?>
         </div>
-
+        
+        <h2 class="menu-subhead">Drinks</h2>
         <div class="menu-container">
-            <h2 class="menu-subhead">Drinks</h2>
+
             <?php
             include 'drink-item.php';
             mysqli_free_result($drink_result);
@@ -48,17 +49,17 @@ include_once __DIR__ . '/header.php';
 </main>
 
 <?php
-    if (isset($_POST['add-item'])) {
-        $menu_item = mysqli_real_escape_string($db_connection, $_POST['id']);
-        $query = "INSERT INTO `cart`(`menu-item`) VALUES ('{$menu_item}')";
-        $db_results = mysqli_query($db_connection, $query);
-        if ($db_results) {
-            /* header('Location: menu-with-cart.php'); */
-            die();
-        } else {
-            echo "Query didn't work";
-        }
+if (isset($_POST['add-item'])) {
+    $menu_item = mysqli_real_escape_string($db_connection, $_POST['id']);
+    $query = "INSERT INTO `cart`(`menu-item`) VALUES ('{$menu_item}')";
+    $db_results = mysqli_query($db_connection, $query);
+    if ($db_results) {
+        /* header('Location: menu-with-cart.php'); */
+        die();
+    } else {
+        echo "Query didn't work";
     }
+}
 ?>
 
 <?php
