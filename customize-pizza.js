15b4db22e5de1c4d2d9cb964ec8fd29eb7fb9294
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-03-16 17:53:37 -0400

    customize page sticky and responsive

--- customize-pizza.js
+++ customize-pizza.js
@@ -1,14 +1,26 @@
 // When the user scrolls down 80px from the top of the document, resize the customized pizza
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  if ($(window).width() < 1200) {
+
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
       // When scrolling
-    document.getElementById("customize-pizza-img").style.width = "50%";
-    document.getElementById("customize-pizza-img").style.margin = "0 0 0 300px";
-    // When unscrolling
+      document.getElementById("customize-pizza-price").style.fontSize = "1rem";
+      document.getElementById("sticky-customize-pizza").style.width = "min-content";
+      document.getElementById("sticky-customize-pizza").style.margin = "0 0 0 auto";
+
+
+
+    } else {
+      // When unscrolling
+      document.getElementById("customize-pizza-price").style.fontSize = "2.25rem";
+      document.getElementById("sticky-customize-pizza").style.width = "max-content";
+      document.getElementById("sticky-customize-pizza").style.margin = "auto";
+    }
   } else {
-    document.getElementById("customize-pizza-img").style.width = "100%";
-    document.getElementById("customize-pizza-img").style.margin = "0";
+    // When it is on desktop
+    document.getElementById("sticky-customize-pizza").style.width = "fit-content";
+    document.getElementById("customize-pizza-price").style.fontSize = "2.25rem";
   }
 }
--- customize-pizza.php
+++ customize-pizza.php
@@ -13,87 +13,90 @@ include_once __DIR__ . '/header.php';
                 <img src="svg/ui-back-arrow.svg" height="32" width="32">
             </a>
         </div>
+        <div class="customize-pizza-grid">
+            <div id="sticky-customize-pizza">
+                <img src="imgs/menu/plain_pizza_menu.png" alt="customize pizza" id="customize-pizza-img">
+                <h1 id="customize-pizza-price">Total Price: $12.00</h1>
+            </div>
 
-        <div class="sticky-customize-pizza">
-            <img src="imgs/menu/plain_pizza_menu.png" alt="customize pizza" id="customize-pizza-img">
+            <div class="topping-container white-background">
+                <form action="customize-pizza-process.php" method="post">
+                    <h2 class="orange-text">Add Toppings</h2>
+                    <h4>Extra Cheese</h4>
+                    <label class="container">
+                        <input type="checkbox" value="Mozzarella" id="mozzarella" name="mozzarella">
+                        <span class="checkmark"></span>Mozzarella
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+                    <label class="container">
+                        <input type="checkbox" value="Parmesan" id="parmesan" name="parmesan">
+                        <span class="checkmark"></span>Parmesan
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+
+                    <h4>Meats</h4>
+                    <label class="container">
+                        <input type="checkbox" value="Pepperoni" id="pepperoni" name="pepperoni">
+                        <span class="checkmark"></span>Pepperoni
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+                    <label class="container">
+                        <input type="checkbox" value="Sausage" id="sausage" name="sausage">
+                        <span class="checkmark"></span>Sausage
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+
+                    <h4>Vegetables</h4>
+                    <label class="container">
+                        <input type="checkbox" value="Spinach" id="spinach" name="spinach">
+                        <span class="checkmark"></span>Spinach
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+                    <label class="container">
+                        <input type="checkbox" value="Arugula" id="arugula" name="arugula">
+                        <span class="checkmark"></span>Arugula
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+                    <label class="container">
+                        <input type="checkbox" value="Rosted Garlic" id="rosted-garlic" name="rosted-garlic">
+                        <span class="checkmark"></span>Rosted Garlic
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+                    <label class="container">
+                        <input type="checkbox" value="Mushrooms" id="mushrooms" name="mushrooms">
+                        <span class="checkmark"></span>Mushrooms
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+                    <label class="container">
+                        <input type="checkbox" value="Cherry Tomato" id="cherry-tomato" name="cherry-tomato">
+                        <span class="checkmark"></span>Cherry Tomato
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+                    <label class="container">
+                        <input type="checkbox" value="Roasted Red Pepper" id="roasted-red-pepper" name="roasted-red-pepper">
+                        <span class="checkmark"></span>Roasted Red Pepper
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+                    <label class="container">
+                        <input type="checkbox" value="Caramelized Onion" id="caramelized-onion" name="caramelized-onion">
+                        <span class="checkmark"></span>Caramelized Onion
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+                    <label class="container">
+                        <input type="checkbox" value="Pear" id="pear" name="pear">
+                        <span class="checkmark"></span>Pear
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+                    <label class="container">
+                        <input type="checkbox" value="Onions" id="onions" name="onions">
+                        <span class="checkmark"></span>Onions
+                        <span class="topping-price">+$1.00</span>
+                    </label>
+                    <input type="submit" value="Done" class="button done-link">
+                </form>
+            </div>
         </div>
 
-        <div class="topping-container white-background">
-            <form action="customize-pizza-process.php" method="post">
-                <h2 class="orange-text">Add Toppings</h2>
-                <h4>Extra Cheese</h4>
-                <label class="container">
-                    <input type="checkbox" value="Mozzarella" id="mozzarella" name="mozzarella">
-                    <span class="checkmark"></span>Mozzarella
-                    <span class="topping-price">+$1.00</span>
-                </label>
-                <label class="container">
-                    <input type="checkbox" value="Parmesan" id="parmesan" name="parmesan">
-                    <span class="checkmark"></span>Parmesan
-                    <span class="topping-price">+$1.00</span>
-                </label>
-
-                <h4>Meats</h4>
-                <label class="container">
-                    <input type="checkbox" value="Pepperoni" id="pepperoni" name="pepperoni">
-                    <span class="checkmark"></span>Pepperoni
-                    <span class="topping-price">+$1.00</span>
-                </label>
-                <label class="container">
-                    <input type="checkbox" value="Sausage" id="sausage" name="sausage">
-                    <span class="checkmark"></span>Sausage
-                    <span class="topping-price">+$1.00</span>
-                </label>
-
-                <h4>Vegetables</h4>
-                <label class="container">
-                    <input type="checkbox" value="Spinach" id="spinach" name="spinach">
-                    <span class="checkmark"></span>Spinach
-                    <span class="topping-price">+$1.00</span>
-                </label>
-                <label class="container">
-                    <input type="checkbox" value="Arugula" id="arugula" name="arugula">
-                    <span class="checkmark"></span>Arugula
-                    <span class="topping-price">+$1.00</span>
-                </label>
-                <label class="container">
-                    <input type="checkbox" value="Rosted Garlic" id="rosted-garlic" name="rosted-garlic">
-                    <span class="checkmark"></span>Rosted Garlic
-                    <span class="topping-price">+$1.00</span>
-                </label>
-                <label class="container">
-                    <input type="checkbox" value="Mushrooms" id="mushrooms" name="mushrooms">
-                    <span class="checkmark"></span>Mushrooms
-                    <span class="topping-price">+$1.00</span>
-                </label>
-                <label class="container">
-                    <input type="checkbox" value="Cherry Tomato" id="cherry-tomato" name="cherry-tomato">
-                    <span class="checkmark"></span>Cherry Tomato
-                    <span class="topping-price">+$1.00</span>
-                </label>
-                <label class="container">
-                    <input type="checkbox" value="Roasted Red Pepper" id="roasted-red-pepper" name="roasted-red-pepper">
-                    <span class="checkmark"></span>Roasted Red Pepper
-                    <span class="topping-price">+$1.00</span>
-                </label>
-                <label class="container">
-                    <input type="checkbox" value="Caramelized Onion" id="caramelized-onion" name="caramelized-onion">
-                    <span class="checkmark"></span>Caramelized Onion
-                    <span class="topping-price">+$1.00</span>
-                </label>
-                <label class="container">
-                    <input type="checkbox" value="Pear" id="pear" name="pear">
-                    <span class="checkmark"></span>Pear
-                    <span class="topping-price">+$1.00</span>
-                </label>
-                <label class="container">
-                    <input type="checkbox" value="Onions" id="onions" name="onions">
-                    <span class="checkmark"></span>Onions
-                    <span class="topping-price">+$1.00</span>
-                </label>
-                <input type="submit" value="Done" class="button done-link">
-            </form>
-        </div>
     </div>
 </main>
 
